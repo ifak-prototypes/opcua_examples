7a68a618ae2f356d9e616e794674501bc553a833
Kefaku <keanuf26@gmail.com>
2023-03-06 19:36:09 +0100

    add setup.py, pyproject.toml and workshop pdf

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(
+    name='opcua-sampleproject',
+    version='1.0.0',
+    description='A sample opcua project',
+    url='https://github.com/<user>/<repo>',
+    packages=['opcua', 'jupyter', 'numpy', 'matplotlib'],
+)
