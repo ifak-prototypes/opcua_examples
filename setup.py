01b8d9de431cd17c17036db2b6129484547c29c0
Kefaku <keanuf26@gmail.com>
2023-03-08 17:17:02 +0100

    update dependencies

--- setup.py
+++ setup.py
@@ -4,6 +4,6 @@ setup(
     name='opcua-sampleproject',
     version='1.0.0',
     description='A sample opcua project',
-    url='https://github.com/<user>/<repo>',
-    packages=['opcua', 'jupyter', 'numpy', 'matplotlib'],
+    url='https://github.com/Kefaku/opcua-example',
+    packages=['opcua', 'jupyter', 'numpy', 'matplotlib', 'pyOpenSSL'],
 )
