b2313de6b1afb9eff0b9795b32e4c239d34a90e1
Kefaku <114934849+Kefaku@users.noreply.github.com>
2023-03-09 11:04:13 +0100

    required dependencies specified in setup.py

--- setup.py
+++ setup.py
@@ -5,5 +5,5 @@ setup(
     version='1.0.0',
     description='A sample opcua project',
     url='https://github.com/Kefaku/opcua-example',
-    packages=['opcua', 'jupyter', 'numpy', 'matplotlib', 'pyOpenSSL'],
+    install_requires=['opcua', 'jupyter', 'numpy', 'matplotlib', 'pyOpenSSL'],
 )
