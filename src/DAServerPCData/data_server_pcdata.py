a8c207e8ed5b4e552e8d1e7c07c9bc120bb1e2b9
Kefaku <114934849+Kefaku@users.noreply.github.com>
2023-03-09 10:47:46 +0100

    complete DAServerPCData example

--- src/DAServerPCData/data_server_pcdata.py
+++ src/DAServerPCData/data_server_pcdata.py
@@ -14,24 +14,27 @@ objects = server.get_objects_node()
 # pc values init
 pcdata = objects.add_object(idx, "PCData")
 
-_cpu = pcdata.add_variable(idx, "CPU", 100)
-_ram = pcdata.add_variable(idx, "RAM", 100)
-_battery = pcdata.add_variable(idx, "Battery", 100)
+_cpu = pcdata.add_variable(idx, "CPU", 0)
+_ram = pcdata.add_variable(idx, "RAM", 0)
+_battery = pcdata.add_variable(idx, "Battery", 0)
 
 # pc values update
 def data_update():
     _cpu.set_value(psutil.cpu_percent())
     _ram.set_value(psutil.virtual_memory().percent)
+    _battery.set_value(int(psutil.sensors_battery().percent))
     _battery.set_value(psutil.sensors_battery().percent)
     print(f"{_cpu.get_value()} / {_ram.get_value()} / {_battery.get_value()}")
+
+data_update()
 
 # server
 server.start()
 
 # history
-server.historize_node_data_change(_cpu, period=None, count=100)
-server.historize_node_data_change(_ram, period=None, count=100)
-server.historize_node_data_change(_battery, period=None, count=100)
+server.historize_node_data_change(_cpu, period=None, count=20)
+server.historize_node_data_change(_ram, period=None, count=20)
+server.historize_node_data_change(_battery, period=None, count=20)
 
 try:
     while True:
